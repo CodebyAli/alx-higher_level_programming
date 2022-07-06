3479926d7761d83e4238c9628c4281aeebb8ed9f
CodebyAli <andhuimedineali@yahoo.fr>
2022-07-06 11:27:53 -0700

    Task 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    def search_replace_row(row):
-	    return (row if row =! search else replace)
-    return list(map(search_research_row, my_list))
+    def s_r_row(row):
+        return (row if row != search else replace)
+    return list(map(s_r_row, my_list))
