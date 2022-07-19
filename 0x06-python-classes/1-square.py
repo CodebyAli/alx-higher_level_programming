da026d96fa9a438c2e453615683b088e535ff82b
CodebyAli <andhuimedineali@yahoo.fr>
2022-07-19 15:35:05 -0700

    Task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,19 +1,14 @@
 #!/usr/bin/python3
-"""Square class definition"""
+# 1-square.py
+"""Square class definition."""
 
 
 class Square:
-    """Represents a square
+    """Represents a square."""
 
-    Attributes:
-        __size (int): size of a side of the square
-    """
     def __init__(self, size):
-        """Initializes a square
-
+        """Initialize a square
         Args:
             size (int): size of a side of the square
-        
-        Returns: None
         """
         self.__size = size
